cd37f300fca3f1d1f09a94f8b1622a5af68c1490
Josemaria <josemarianriagu25@gmail.com>
2019-05-14 14:47:30 +0100

    - Add event listeners to window and img_content

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@
 const body = document.body;
 const textlogo = document.querySelector('.logo-heading');
 const headerimage = document.querySelector('.intro img');
-const nav_anchor = document.querySelectorAll('.nav-link');
-const img_content = document.querySelectorAll('.img-content img');
-const h2 = document.querySelectorAll('h2');
+const anchor1 = document.querySelector('a:nth-of-type(1)');
+const img_content = Array.from(document.querySelectorAll('.img-content img'));
+const h2 = Array.from(document.querySelectorAll('h2'));
 const footer = document.querySelector('footer');
 
 // Run specified listeners once
@@ -28,15 +28,41 @@ function enter() {
     setTimeout(() => {
         textlogo.textContent = 'Fun Bus';
         body.style.backgroundColor = "";
-    }, 3000);
+    }, 500);
 }
 function remove_img() {
     headerimage.setAttribute('src', '#');
+    textlogo.textContent = 'Oops! No Image'
     setTimeout(() => {
         headerimage.setAttribute('src', 'img/fun-bus.jpg')
+        textlogo.textContent = 'Fun Bus'
     }, 3000);
+}
+function resize() {
+    h2[0].textContent = 'Window width is now ' + window.innerWidth + ' px';
+    setTimeout(() => {
+        h2[0].textContent = 'Welcome To Fun Bus';
+    }, 3000);
+}
+function logKey(e) {
+    anchor1.preventDefault();
+    textlogo.textContent = ` ${e.code}`;
+    setTimeout(() => {
+        textlogo.textContent = 'Fun Bus';;
+    }, 3000);
+}
+function swap() {
+    img_content[0].setAttribute('src', 'img/fun.jpg');
+    img_content[1].setAttribute('src', '/img/adventure.jpg');
+    setTimeout(() => {
+        img_content[0].setAttribute('src', 'img/adventure.jpg');
+    img_content[1].setAttribute('src', 'img/fun.jpg');
+    }, 1000);
+    
 }
 
 // Add event listeners to our playground
 body.addEventListener('mouseenter', enter);
 headerimage.addEventListener('drag', remove_img);
+window.addEventListener('resize', resize);
+img_content[0].addEventListener('dblclick',swap);
