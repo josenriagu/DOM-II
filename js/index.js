ddf116ac115715eea9cacea0ac42b10df4988a3e
Josemaria <josemarianriagu25@gmail.com>
2019-05-14 13:41:33 +0100

    - Add event listeners to  body and headerimage

--- js/index.js
+++ js/index.js
@@ -9,4 +9,34 @@ const img_content = document.querySelectorAll('.img-content img');
 const h2 = document.querySelectorAll('h2');
 const footer = document.querySelector('footer');
 
+// Run specified listeners once
+
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+
+function enter() {
+    textlogo.textContent = 'Hello Fun Mouse';
+    body.style.backgroundColor = "#dddddd";
+    setTimeout(() => {
+        textlogo.textContent = 'Fun Bus';
+        body.style.backgroundColor = "";
+    }, 3000);
+}
+function remove_img() {
+    headerimage.setAttribute('src', '#');
+    setTimeout(() => {
+        headerimage.setAttribute('src', 'img/fun-bus.jpg')
+    }, 3000);
+}
+
 // Add event listeners to our playground
+body.addEventListener('mouseenter', enter);
+headerimage.addEventListener('drag', remove_img);
