2a7b4ed2ca956734e66aa972e6299117cffd9d20
Josemaria <josemarianriagu25@gmail.com>
2019-05-14 12:17:35 +0100

    - Define selector variables to play with

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+;// Your code goes here
+
+// Grab elements to play with
+const body = document.body;
+const textlogo = document.querySelector('.logo-heading');
+const headerimage = document.querySelector('.intro img');
+const nav_anchor = document.querySelectorAll('.nav-link');
+const img_content = document.querySelectorAll('.img-content img');
+const h2 = document.querySelectorAll('h2');
+const footer = document.querySelector('footer');
+
+// Add event listeners to our playground
